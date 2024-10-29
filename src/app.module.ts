803799d54bdf879504cd2a794eaff69a499b80d1
mushtariy93 <abdurashidovamushtariykumush@gmail.com>
2024-10-29 10:12:41 +0500

    Added deploy_yml2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), AuthModule],
